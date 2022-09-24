3d59139c250d3235f26d4590f2312ccef9c007a0
Ngoc Phuong Khanh Le <le000222@algonquinlive,com>
2022-09-24 19:41:26 -0400

    Change typedef names in compilers.h

---
+++ Compilers.h
@@ -0,0 +1,100 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Fall, 2022
+* Author: Svillen Ranev - Paulo Sousa
+* Professors: Paulo Sousa
+************************************************************
+ _________________________________
+|                                 |
+| ........ BOA LANGUAGE ......... |
+|     __    __    __    __        |
+|    /  \  /  \  /  \  /  \       |
+| __/  __\/  __\/  __\/  __\__    |
+| _/  /__/  /__/  /__/  /_____|   |
+|  \_/ \   / \   / \   / \  \___  |
+|       \_/   \_/   \_/   \___o_> |
+|                                 |
+| .. ALGONQUIN COLLEGE - 2022F .. |
+|_________________________________|
+
+*/
+
+/*
+************************************************************
+* File name: compilers.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Assignment: A12, A22, A32.
+* Date: Sep 01 2022
+* Professor: Paulo Sousa
+* Purpose: This file defines the functions called by main function.
+* Function list: mainReader(), mainScanner(), mainParser().
+*************************************************************/
+
+#ifndef COMPILERS_H_
+#define COMPILERS_H_
+
+#define DEBUG 0
+
+/*
+ * ............................................................................
+ * ADVICE 1:
+ * Please check the "TODO" labels to develop your activity.
+ *
+ * ADVICE 2: This file must be updated according to each assignment
+ * (see the progression: reader > scanner > parser).
+ * ............................................................................
+ */
+ 
+/* Logical constants - adapt for your language */
+#define RID_TRUE  1
+#define RID_FALSE 0
+
+/*
+------------------------------------------------------------
+Data types definitions
+NOTE: Some types may not be directly used by your language,
+		but they can be necessary for conversions.
+------------------------------------------------------------
+*/
+/* TO_DO: Define your typedefs */
+typedef char			rid_char;
+typedef int				rid_int;
+typedef float			rid_float;
+typedef void			rid_void;
+
+typedef unsigned char	rid_boln;
+typedef unsigned char	rid_byte;
+
+typedef long			rid_long;
+typedef double			rid_double;
+
+/*
+------------------------------------------------------------
+Programs:
+1: Reader - invokes MainReader code
+2: Scanner - invokes MainScanner code
+3: Parser - invokes MainParser code
+------------------------------------------------------------
+*/
+enum PROGRAMS {
+	PGM_READER	= 'R',
+	PGM_SCANNER = 'S',
+	PGM_PARSER	= 'P'
+};
+
+/*
+------------------------------------------------------------
+Main functions signatures
+(Code will be updated during next assignments)
+------------------------------------------------------------
+*/
+rid_int mainReader  (rid_int argc, rid_char** argv);
+/*
+boa_intg mainScanner (boa_intg argc, boa_char** argv);
+boa_intg mainParser  (boa_intg argc, boa_char** argv);
+*/
+rid_void printLogo();
+
+#endif
---
+++ Reader.c
@@ -0,0 +1,592 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Fall, 2022
+* Author: Svillen Ranev - Paulo Sousa
+* Professors: Paulo Sousa
+************************************************************
+ _________________________________
+|                                 |
+| ........ BOA LANGUAGE ......... |
+|     __    __    __    __        |
+|    /  \  /  \  /  \  /  \       |
+| __/  __\/  __\/  __\/  __\__    |
+| _/  /__/  /__/  /__/  /_____|   |
+|  \_/ \   / \   / \   / \  \___  |
+|       \_/   \_/   \_/   \___o_> |
+|                                 |
+| .. ALGONQUIN COLLEGE - 2022F .. |
+|_________________________________|
+
+*/
+
+/*
+***********************************************************
+* File name: Reader.c
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Assignment: A12.
+* Date: Sep 01 2022
+* Professor: Paulo Sousa
+* Purpose: This file is the main code for Buffer/Reader (A12)
+************************************************************
+*/
+
+/*
+ *.............................................................................
+ * MAIN ADVICE:
+ * - Please check the "TODO" labels to develop your activity.
+ * - Review the functions to use "Defensive Programming".
+ *.............................................................................
+ */
+
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+
+#ifndef READER_H_
+#include "Reader.h"
+#endif
+
+/*
+***********************************************************
+* Function name: readerCreate
+* Purpose: Creates the buffer reader according to capacity, increment
+	factor and operational mode ('f', 'a', 'm')
+* Author: Svillen Ranev / Paulo Sousa
+* History/Versions: S22
+* Called functions: calloc(), malloc()
+* Parameters:
+*   size = initial capacity
+*   increment = increment factor
+*   mode = operational mode
+* Return value: bPointer (pointer to reader)
+* Algorithm: Allocation of memory according to inicial (default) values.
+* TODO ......................................................
+*	- Adjust datatypes for your LANGUAGE.
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Check flags.
+*************************************************************
+*/
+
+ReaderPointer readerCreate(rid_int size, rid_int increment, rid_int mode) {
+	ReaderPointer readerPointer;
+	/* TO_DO: Defensive programming */
+	if (size == 0 || increment == 0 || mode == 0) {
+		return NULL;
+	}
+	/* TO_DO: Adjust the values according to parameters */
+	if (size < 0  || increment < 0 || mode)
+	size = READER_DEFAULT_SIZE;
+	increment = READER_DEFAULT_INCREMENT;
+	mode = MODE_FIXED;
+	readerPointer = (ReaderPointer)calloc(1, sizeof(BufferReader));
+	/* TO_DO: Defensive programming */
+	readerPointer->content = (rid_char*)malloc(size);
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Initialize the histogram */
+	readerPointer->size = size;
+	readerPointer->increment = increment;
+	readerPointer->mode = mode;
+	/* TO_DO: Initialize flags */
+	/* TO_DO: The created flag must be signalized as EMP */
+	return readerPointer;
+}
+
+
+/*
+***********************************************************
+* Function name: readerAddChar
+* Purpose: Adds a char to buffer reader
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+*   ch = char to be added
+* Return value:
+*	readerPointer (pointer to Buffer Reader)
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+
+ReaderPointer readerAddChar(ReaderPointer const readerPointer, rid_char ch) {
+	rid_char* tempReader = NULL;
+	rid_int newSize = 0;
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Reset Realocation */
+	/* TO_DO: Test the inclusion of chars */
+	if (readerPointer->position.wrte * (rid_int)sizeof(rid_char) < readerPointer->size) {
+		/* TO_DO: This buffer is NOT full */
+	} else {
+		/* TO_DO: Reset Full flag */
+		switch (readerPointer->mode) {
+		case MODE_FIXED:
+			return NULL;
+		case MODE_ADDIT:
+			/* TO_DO: Adjust new size */
+			/* TO_DO: Defensive programming */
+			break;
+		case MODE_MULTI:
+			/* TO_DO: Adjust new size */
+			/* TO_DO: Defensive programming */
+			break;
+		default:
+			return NULL;
+		}
+		/* TO_DO: New reader allocation */
+		/* TO_DO: Defensive programming */
+		/* TO_DO: Check Relocation */
+	}
+	/* TO_DO: Add the char */
+	readerPointer->content[readerPointer->position.wrte++] = ch;
+	/* TO_DO: Updates histogram */
+	return readerPointer;
+}
+
+/*
+***********************************************************
+* Function name: readerClear
+* Purpose: Clears the buffer reader
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Boolean value about operation success
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_boln readerClear(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Adjust flags original */
+	return RID_TRUE;
+}
+
+/*
+***********************************************************
+* Function name: readerFree
+* Purpose: Releases the buffer address
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Boolean value about operation success
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_boln readerFree(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	if (!readerPointer)
+		return RID_FALSE;
+	/* TO_DO: Free pointers */
+	return RID_TRUE;
+}
+
+/*
+***********************************************************
+* Function name: readerIsFull
+* Purpose: Checks if buffer reader is full
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Boolean value about operation success
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_boln readerIsFull(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Check flag if buffer is FUL */
+	return RID_FALSE;
+}
+
+
+/*
+***********************************************************
+* Function name: readerIsEmpty
+* Purpose: Checks if buffer reader is empty.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Boolean value about operation success
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_boln readerIsEmpty(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Check flag if buffer is EMP */
+	return RID_FALSE;
+}
+
+/*
+***********************************************************
+* Function name: readerSetMark
+* Purpose: Adjust the position of mark in the buffer
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+*   mark = mark position for char
+* Return value:
+*	Boolean value about operation success
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_boln readerSetMark(ReaderPointer const readerPointer, rid_int mark) {
+	/* TO_DO: Defensive programming */
+	if (!readerPointer || mark<0 || mark > readerPointer->position.wrte)
+		return RID_FALSE;
+	/* TO_DO: Adjust mark */
+	readerPointer->position.mark = mark;
+	return RID_TRUE;
+}
+
+
+/*
+***********************************************************
+* Function name: readerPrint
+* Purpose: Prints the string in the buffer.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Number of chars printed.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_int readerPrint(ReaderPointer const readerPointer) {
+	rid_int cont = 0;
+	rid_char c;
+	/* TO_DO: Defensive programming (including invalid chars) */
+	c = readerGetChar(readerPointer);
+	/* TO_DO: Check flag if buffer EOB has achieved */
+	while (cont < readerPointer->position.wrte) {
+		cont++;
+		printf("%c", c);
+		c = readerGetChar(readerPointer);
+	}
+	return cont;
+}
+
+/*
+***********************************************************
+* Function name: readerLoad
+* Purpose: Loads the string in the buffer with the content of
+	an specific file.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+*   fileDescriptor = pointer to file descriptor
+* Return value:
+*	Number of chars read and put in buffer.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_int readerLoad(ReaderPointer const readerPointer, FILE* const fileDescriptor) {
+	rid_int size = 0;
+	rid_char c;
+	/* TO_DO: Defensive programming */
+	c = (rid_char)fgetc(fileDescriptor);
+	while (!feof(fileDescriptor)) {
+		if (!readerAddChar(readerPointer, c)) {
+			ungetc(c, fileDescriptor);
+			return READER_ERROR;
+		}
+		c = (char)fgetc(fileDescriptor);
+		size++;
+	}
+	/* TO_DO: Defensive programming */
+	return size;
+}
+
+
+/*
+***********************************************************
+* Function name: readerRecover
+* Purpose: Rewinds the buffer.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value
+*	Boolean value about operation success
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_boln readerRecover(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Recover positions */
+	return RID_TRUE;
+}
+
+
+/*
+***********************************************************
+* Function name: readerRetract
+* Purpose: Retracts the buffer to put back the char in buffer.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Boolean value about operation success
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_boln readerRetract(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Retract (return 1 pos read) */
+	return RID_TRUE;
+}
+
+
+/*
+***********************************************************
+* Function name: readerRestore
+* Purpose: Resets the buffer.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Boolean value about operation success
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_boln readerRestore(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Restore positions (read/mark) */
+	return RID_TRUE;
+}
+
+
+
+/*
+***********************************************************
+* Function name: readerGetChar
+* Purpose: Returns the char in the getC position.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Char in the getC position.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_char readerGetChar(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Check condition to read/wrte */
+	/* TO_DO: Set EOB flag */
+	/* TO_DO: Reset EOB flag */
+	return readerPointer->content[readerPointer->position.read++];
+}
+
+
+/*
+***********************************************************
+* Function name: readerGetContent
+* Purpose: Returns the pointer to String.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+*   pos = position to get the pointer
+* Return value:
+*	Position of string char.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_char* readerGetContent(ReaderPointer const readerPointer, rid_int pos) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Return content (string) */
+	return NULL;
+}
+
+
+
+/*
+***********************************************************
+* Function name: readerGetPosRead
+* Purpose: Returns the value of getCPosition.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	The read position offset.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_int readerGetPosRead(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Return read */
+	return 0;
+}
+
+
+/*
+***********************************************************
+* Function name: readerGetPosWrte
+* Purpose: Returns the position of char to be added
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Write position
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_int readerGetPosWrte(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Return wrte */
+	return 0;
+}
+
+
+/*
+***********************************************************
+* Function name: readerGetPosMark
+* Purpose: Returns the position of mark in the buffer
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Mark position.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_int readerGetPosMark(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Return mark */
+	return 0;
+}
+
+
+/*
+***********************************************************
+* Function name: readerGetSize
+* Purpose: Returns the current buffer capacity
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Size of buffer.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_int readerGetSize(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Return size */
+	return 0;
+}
+
+/*
+***********************************************************
+* Function name: readerGetInc
+* Purpose: Returns the buffer increment.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	The Buffer increment.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_int readerGetInc(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Return increment */
+	return 0;
+}
+
+/*
+***********************************************************
+* Function name: readerGetMode
+* Purpose: Returns the operational mode
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Operational mode.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_int readerGetMode(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Return mode */
+	return 0;
+}
+
+
+/*
+***********************************************************
+* Function name: readerGetFlags
+* Purpose: Returns the entire flags of Buffer.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Flags from Buffer.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_byte readerGetFlags(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Return flags */
+	return 0;
+}
+
+
+/*
+***********************************************************
+* Function name: readerShowStat
+* Purpose: Returns the number of chars used.
+* Parameters:
+*   readerPointer = pointer to Buffer Reader
+* Return value:
+*	Number of chars.
+* TO_DO:
+*   - Use defensive programming
+*	- Check boundary conditions
+*	- Adjust for your LANGUAGE.
+*************************************************************
+*/
+rid_int readerShowStat(ReaderPointer const readerPointer) {
+	/* TO_DO: Defensive programming */
+	/* TO_DO: Updates the histogram */
+	return 0;
+}
